571b2e246e7eedcdc76e2b628b0cd94a0d4f94a1
YaKnee <jani.oc@hotmail.com>
2024-03-21 16:52:14 +0200

    refactor: add country to location object

--- app/src/main/java/orders/Location.java
+++ app/src/main/java/orders/Location.java
@@ -1,6 +1,6 @@
 package orders;
 
-public record Location(String city, String state, String Region) {
+public record Location(String country, String city, String state, String postCode, String Region) {
 //    private String city;
 //    private String state;
 //    private String Region;
