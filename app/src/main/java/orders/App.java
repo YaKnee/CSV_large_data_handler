84253e86e2111b6ad06fc5ec966173d966f614a6
YaKnee <jani.oc@hotmail.com>
2024-03-21 17:46:22 +0200

    feat: add calculate customers per state

--- app/src/main/java/orders/App.java
+++ app/src/main/java/orders/App.java
@@ -11,27 +11,69 @@ import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.io.FileReader;
-import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+//TO DO
+////Add Threading for Buttons
+////Selector for States/Customers + Search bars
+////View summary of orders of customer
+////Average Sales amounts of the orders                 (done?)
+////Customer with most sales
+////Count segments
+////total sales per year
+////total sales per region
+////****Testing****
+////JavaDocs
+////Make Pretty
 
 public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         ArrayList<Order> orders = readCSVData("data/SuperStoreOrders.csv");
+        Set<Location> uniqueLocations = extractLocations(orders);
 
-        ///////////////////////////////////////////////////////////mess around with other layouts
+        Label stateLabel=new Label("State:"); 
+        TextField stateInput=new TextField();
+        Button stateSearchBtn = new Button("Search");
+        stateSearchBtn.getStyleClass().add(".button");
+        Label stateOutput = new Label();
 
+        stateSearchBtn.setOnAction(e-> {
+            String state = stateInput.getText();
+            if (state == null || state.isEmpty()) {
+                stateOutput.setText("Empty search bar.");
+            }else {
+                boolean stateExists = uniqueLocations.stream()
+                .anyMatch(location -> state.equalsIgnoreCase(location.state()));
+                if (stateExists) {
+                    int customersInState = (int) orders.stream()
+                    .filter(order -> state.equalsIgnoreCase(order.location().state()))
+                    .count();
+                    System.out.println("Customers in " + toProperCase(state) + ": " + customersInState);
+                    stateOutput.setText("Customers in " + toProperCase(state) + ": " + customersInState);
+                } else {
+                    System.out.println("State not found.");
+                    stateOutput.setText("State not found.");
+                }
+            }
+        });
 
-        Label cityLabel=new Label("City:"); 
-        TextField cityInput=new TextField();
-        Button searchBtn = new Button("Search");
-        searchBtn.getStyleClass().add(".button");
-        Label cityOutput = new Label();
-        searchBtn.setOnAction(e-> {
-            System.out.println("You entered City: " + cityInput.getText());
-            cityOutput.setText(toProperCase(cityInput.getText()));
+        Label customerLabel=new Label("Customer Count:"); 
+        TextField customerInput=new TextField();
+        Button customerSearchBtn = new Button("Search");
+        customerSearchBtn.getStyleClass().add(".button");
+        Label customerOutput = new Label();
+        customerSearchBtn.setOnAction(e-> {
+            if (customerInput.getText() == null || customerInput.getText().isEmpty()) { ////////////////or not found in search
+                customerOutput.setText("Unknown Customer");
+            }else {
+                
+                System.out.println("You entered Customer: " + customerInput.getText());
+                customerOutput.setText(toProperCase(customerInput.getText()));
+            }
         });
 
         Button salesBtn = new Button("Average Sales");
@@ -42,36 +84,24 @@ public class App extends Application{
             System.out.println("Total Average Sales: " + averageSales);
         });
 
-        ////////////////////////////////////////////////////////////Change to a search/select
-        Button customerPerStateBtn = new Button("Customers in Kentucky");
-        customerPerStateBtn.getStyleClass().add(".button");
-        customerPerStateBtn.setOnAction(e -> {
-            int kentuckyCount = (int) orders.stream()
-                    .map(Order::location)
-                    .filter(location -> "Kentucky".equals(location.state()))
-                    .count();
-            System.out.println("Customers in Kentucky: " + kentuckyCount);
-        });
-
-
+        ///////////////////////////////////////////////////////////mess around with other layouts
         GridPane root = new GridPane();
         root.setMinSize(400, 200);
         root.setPadding(new Insets(10, 10, 10, 10)); 
         root.setVgap(5); 
         root.setHgap(5);       
         root.setAlignment(Pos.CENTER); 
-        root.add(cityLabel, 0, 0);
-        root.add(cityInput, 1, 0);
-        root.add(cityOutput, 2, 0);
-        root.add(searchBtn, 0, 1);
+
+        root.add(stateLabel, 0, 0);
+        root.add(stateInput, 1, 0);
+        root.add(stateSearchBtn, 2, 0);
+        root.add(stateOutput, 1, 1);
         root.add(salesBtn, 1, 2);
-        root.add(customerPerStateBtn, 1, 3);
-        cityOutput.setStyle("-fx-background-colour: #fffaf0;");
-        root.setStyle("-fx-background-colour: #6495ed");
-        // root.addRow(0,cityLabel,cityInput,searchBtn,cityOutput);
-        // root.addRow(1, salesBtn);
-        // root.addRow(2, customerPerStateBtn);
-        // root.getChildren().addAll(cityLabel,cityInput,searchBtn, cityOutput,salesBtn, customerPerStateBtn);
+        root.add(customerLabel, 0, 3);
+        root.add(customerInput, 1, 3);
+        root.add(customerSearchBtn, 2, 3);
+        root.add(customerOutput, 1, 4);
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add("stylesheet.css");
         primaryStage.setScene(scene);
@@ -84,6 +114,7 @@ public class App extends Application{
         //System.out.println(new App().getGreeting());
         //List<Order> orders = readCSVData("data/SuperStoreOrders.csv");
     }
+
     private static ArrayList<Order> readCSVData(String file) {
         ArrayList<Order> allOrders = new ArrayList<>();
         try (CSVReader csvReader = new CSVReaderBuilder(new FileReader(file))
@@ -92,21 +123,23 @@ public class App extends Application{
                 .build()) {
             csvReader.iterator().forEachRemaining(order -> {
                 try {
+                    int rowID = Integer.parseInt(order[0]);
+                                                            //orderId, orderDate, shipDate, Ship Mode
+                    ShippingOrder shipOrder = new ShippingOrder(order[1],order[2],order[3], order[4]);
                                                     //customerId, Name, Segment
                     Customer customer = new Customer(order[5], order[6], order[7]);
-                    String orderDate = order[2];
-                    String shippingMode = order[4];
-                                                    //City,     State,      Region
-                    Location location = new Location(order[9], order[10], order[12]);
+
+                                                    //Country, state,     State,    Post Code,  Region
+                    Location location = new Location(order[8], order[9], order[10], order[11], order[12]);
                                                 //productId, category, sub-cat,      name
                     Product product = new Product(order[13], order[14], order[15], order[16]);
                     //Commas were randomly interpolated in the data that weren't shown in Excel,
                     //so replace with blanks
-                    int sales = replaceCommaAndParse(order[17], order[0]);
-                    int quantity = replaceCommaAndParse(order[18], order[0]);
-                    int discount = replaceCommaAndParse(order[19], order[0]);
-                    int profit = replaceCommaAndParse(order[20], order[0]);
-                    Order fullOrder = new Order(customer, orderDate, shippingMode, location,
+                    int sales = replaceCommaAndParse(order[17], rowID);
+                    int quantity = replaceCommaAndParse(order[18], rowID);
+                    int discount = replaceCommaAndParse(order[19], rowID);
+                    int profit = replaceCommaAndParse(order[20], rowID);
+                    Order fullOrder = new Order(shipOrder, customer, location,
                             product, sales, quantity, discount, profit);
                     allOrders.add(fullOrder);
                 } catch (NumberFormatException e) {
@@ -118,7 +151,8 @@ public class App extends Application{
         }
         return allOrders;
     }
-    private static int replaceCommaAndParse(String orderItem, String rowID){
+
+    private static int replaceCommaAndParse(String orderItem, int rowID){
         try{
             return Integer.parseInt(orderItem.replace(",",""));
         }catch (NumberFormatException e){
@@ -126,11 +160,17 @@ public class App extends Application{
             return 0; ///////////////////////////////////////////////////////////////////////////Change to Optional?
         }
     }
-
-    public static String toProperCase(String input) { 
-        if (input == null || input.isEmpty()) {
-            throw new IllegalArgumentException("Input string cannot be null or empty");
+    private static Set<Location> extractLocations(ArrayList<Order> allOrders) {
+        Set<Location> uniqueLocations = new HashSet<>();
+        for (Order order : allOrders) {
+            uniqueLocations.add(order.location());
         }
+        return uniqueLocations;
+    }
+    
+
+    //Maybe dont need this if I toLowerCase for search and then output the country/customer from set...
+    private static String toProperCase(String input) {
         StringBuilder properCase = new StringBuilder();
         boolean makeUpperCase = true;
         for (char c : input.toCharArray()) {
@@ -144,8 +184,6 @@ public class App extends Application{
                 } else {
                     properCase.append(Character.toLowerCase(c));
                 }
-            } else {
-                throw new IllegalArgumentException("Input contains special characters/numbers: " + c);
             }
         }
         return properCase.toString();
