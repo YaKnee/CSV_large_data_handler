48ba80a9cc57ad2a6efbb434141a1c3dba328632
YaKnee <jani.oc@hotmail.com>
2024-04-07 17:15:08 +0300

    feat: remove chart zoom
    style lines for checkstyle also

--- app/src/main/java/orders/GUI/EventController.java
+++ app/src/main/java/orders/GUI/EventController.java
@@ -6,59 +6,20 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import orders.OrderObjects.Order;
-
 
 /**
  * Handles events for GUI components.
  */
 public class EventController {
-
-    // /**
-    //  * Number instance for US locale.
-    //  */
-    // private final static NumberFormat numberFormat =
-    //     NumberFormat.getNumberInstance(Locale.US);
-
-
-    // public static <T extends Number> void searchButton(Map<String, T> map,
-    //             Button btn, ComboBox<String> cb, Label output, String item,
-    //             String criteria) {
-    //     btn.setOnAction(e-> {
-    //         String input = cb.getEditor().getText();
-    //         if (input == null || input.isEmpty()) {
-    //             output.setText("Empty search bar.");
-    //         }  else {
-    //             boolean found = false;
-    //             for (String key : map.keySet()) {
-    //                 if (key.equalsIgnoreCase(input)) {
-    //                     output.setText(item + " in " + key + ": " +
-    //                             numberFormat.format(map.get(key)));
-    //                     System.out.println(item + " in " + key + ": " +
-    //                             numberFormat.format(map.get(key)));
-    //                     found = true;
-    //                     break;
-    //                 }
-    //             }
-    //             if (!found) {
-    //                 System.out.println(item + " not found in " + criteria + ": \"" + input + "\".");
-    //                 output.setText(item + " not found in " + criteria + ": \"" + input + "\".");
-    //             }
-    //         }
-    //     /////////////////////////////////////////////////////////////CHECK IF INPUT IS WRONG TYPE
-    //     });
-    // }
 
     /**
      * Populates a TableView with order summary information based on the 
@@ -88,6 +49,7 @@ public class EventController {
                     FXCollections.observableArrayList(
                             orders.get(nameAndID.get(name)));
                     Components.populateSummaryTable(table, data);
+                    cb.setValue("");
                     return;
                 }
             }
@@ -105,33 +67,47 @@ public class EventController {
      * @param orders List of orders to generate the data from
      * @return A LineChart displaying data for given parent and child categories
      */
-    public static LineChart<String, Number> createChartForCategory(String parent, String child, ArrayList<Order> orders) {
+    public static LineChart<String, Number>
+        createChartForCategory(String parent, String child,
+                                ArrayList<Order> orders) {
         final Map<String, ? extends Number> dataMap;
         switch (child) {
             case "City":
                 dataMap = (parent.equals("Customers")) 
-                    ? generateCustomersMap(orders, order -> order.location().city()) 
-                    : generateSalesMap(orders, order -> order.location().city());
+                    ? generateCustomersMap(orders, order ->
+                        order.location().city()) 
+                    : generateSalesMap(orders, order ->
+                        order.location().city());
                 break;
             case "State":
                 dataMap = (parent.equals("Customers")) 
-                    ? generateCustomersMap(orders, order -> order.location().state()) 
-                    : generateSalesMap(orders, order -> order.location().state());
+                    ? generateCustomersMap(orders, order ->
+                        order.location().state()) 
+                    : generateSalesMap(orders, order ->
+                        order.location().state());
                 break;
             case "Region":
                 dataMap = (parent.equals("Customers")) 
-                    ? generateCustomersMap(orders, order -> order.location().region()) 
-                    : generateSalesMap(orders, order -> order.location().region());
+                    ? generateCustomersMap(orders, order ->
+                        order.location().region()) 
+                    : generateSalesMap(orders, order ->
+                        order.location().region());
                 break;
             case "Segment":
                 dataMap = (parent.equals("Customers")) 
-                    ? generateCustomersMap(orders, order -> order.customer().segment()) 
-                    : generateSalesMap(orders, order -> order.customer().segment());
+                    ? generateCustomersMap(orders,order ->
+                        order.customer().segment())
+                    : generateSalesMap(orders, order ->
+                        order.customer().segment());
                 break;
             case "Year":
                 dataMap = (parent.equals("Customers")) 
-                    ? generateCustomersMap(orders, order -> order.shipOrder().orderDate().substring(order.shipOrder().orderDate().length() - 4)) 
-                    : generateSalesMap(orders, order -> order.shipOrder().orderDate().substring(order.shipOrder().orderDate().length() - 4));
+                    ? generateCustomersMap(orders, order ->
+                        order.shipOrder().orderDate().substring(
+                            order.shipOrder().orderDate().length() - 4))
+                    : generateSalesMap(orders, order ->
+                        order.shipOrder().orderDate().substring(
+                            order.shipOrder().orderDate().length() - 4));
                 break;
             default:
                 dataMap = new HashMap<>();
@@ -168,32 +144,5 @@ public class EventController {
                         Collectors.mapping(order -> 
                             order.customer().name(), /////////////////////////////change back to customerID?
                             Collectors.summingInt(x -> 1))));
-    }
-
-
-    /**
-     * Adds zoom functionality to the provided LineChart.
-     *
-     * @param chart The LineChart to which zoom functionality is added.
-     */
-    public static void handleChartZoom(LineChart<String, Number> chart) {
-        final double scalingFactor = 1.1;
-        chart.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                double scaleFactor = (event.getButton() == MouseButton.PRIMARY)
-                                     ? scalingFactor : 1 / scalingFactor;
-                chart.setScaleX(chart.getScaleX() * scaleFactor);
-                chart.setScaleY(chart.getScaleY() * scaleFactor);
-                for (XYChart.Series<String, Number> series : chart.getData()) {
-                    series.getNode().setStyle("-fx-stroke-width:" + scaleFactor + "px");
-                    for (XYChart.Data<String, Number> dataPoint : series.getData()) {
-                        dataPoint.getNode().setScaleX(1 / chart.getScaleX());
-                        dataPoint.getNode().setScaleY(1 / chart.getScaleY());
-                    }
-                }
-                event.consume();
-            }
-        });
     }
 }
