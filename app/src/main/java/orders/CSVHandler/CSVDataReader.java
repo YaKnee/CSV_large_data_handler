ef47a733d6e4c1c83fbdc6add41e337f93f45ba7
YaKnee <jani.oc@hotmail.com>
2024-04-09 14:05:17 +0300

    refactor: break up code into smaller methods
    add better (probably unnecessary) exception handling

--- app/src/main/java/orders/CSVHandler/CSVDataReader.java
+++ app/src/main/java/orders/CSVHandler/CSVDataReader.java
@@ -3,8 +3,11 @@ package orders.CSVHandler;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import javafx.scene.control.Label;
 import orders.OrderObjects.*;
 
 
@@ -21,97 +24,114 @@ public class CSVDataReader {
      * @return List of Order objects
      * @throws Exception If there is an error reading file or parsing data
      */
-    public static ArrayList<Order> createOrders(String file) {
+    public static ArrayList<Order> createOrders(String file, Label error) {
         ArrayList<Order> allOrders = new ArrayList<>();
-        try (CSVReader csvReader = new CSVReaderBuilder(new FileReader(file))
-                .withSkipLines(1)
-                .withCSVParser(new CSVParserBuilder().withSeparator(';').build())
-                .build()) {
+        try (CSVReader csvReader = createCSVReader(file, error)) {
             csvReader.iterator().forEachRemaining(order -> {
                 try {
-                    int rowID = Integer.parseInt(order[0]);
-                                                            //orderId, orderDate, shipDate, Ship Mode
-                    ShippingOrder shipOrder = new ShippingOrder(order[1],order[2],order[3], order[4]);
-                                                    //customerId, Name, Segment
-                    Customer customer = new Customer(order[5], order[6], order[7]);
-                                                    //Country, City,     State,    Post Code,  Region
-                    Location location = new Location(order[8], order[9], order[10], order[11], order[12]);
-                                                //productId, category, sub-category, name
-                    Product product = new Product(order[13], order[14], order[15], order[16]);
-                    int sales = replaceCommaAndParse(order[17], rowID);
-                    int quantity = replaceCommaAndParse(order[18], rowID);
-                    int discount = replaceCommaAndParse(order[19], rowID);
-                    int profit = replaceCommaAndParse(order[20], rowID);
-                    Order fullOrder = new Order(rowID, shipOrder, customer, location,
-                            product, sales, quantity, discount, profit);
-                    allOrders.add(fullOrder);
+                    parseOrderObjects(allOrders, order);
                 } catch (NumberFormatException e) {
-                    System.err.println("Failed to parse data in Row ID: "
+                    setErrorLabel(error, "Failed to parse data in Row ID: "
                                         + order[0]);
                 }
             });
         } catch (Exception e) {
-            System.err.println("Failed to read the CSV file.");
+            setErrorLabel(error, "Failed to read the CSV file: " + file);
             e.printStackTrace();
         }
         return allOrders;
     }
 
 
-    public static ArrayList<Order> createReturns(String file) {
+    public static ArrayList<Order> createReturns(String file, Label error) {
         ArrayList<String> returnIDs = new ArrayList<>();
-        try (CSVReader csvReader = new CSVReaderBuilder(new FileReader(file))
-        .withSkipLines(1)
-        .withCSVParser(new CSVParserBuilder().withSeparator(';').build())
-        .build()) {
+        try (CSVReader csvReader = createCSVReader(file, error)) {
              csvReader.iterator().forEachRemaining(returnId -> {
             try {
                 returnIDs.add(returnId[1]);
-            } catch (NumberFormatException e) {
-                System.err.println("Failed to parse data");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                setErrorLabel(error, "Failed to parse data: index out of bounds.");
             }
          });
-        } catch (Exception e) {
-            System.err.println("Failed to read the Return CSV file.");
+        } catch (IOException e) {
+            setErrorLabel(error, "Failed to read the Return CSV file.");
             e.printStackTrace();
         }
 
         ArrayList<Order> allReturns = new ArrayList<>();
-        try (CSVReader csvReader = new CSVReaderBuilder(new FileReader("data/SuperStoreOrders.csv"))
-                .withSkipLines(1)
-                .withCSVParser(new CSVParserBuilder().withSeparator(';').build())
-                .build()) {
+        try (CSVReader csvReader = createCSVReader("data/SuperStoreOrders.csv", error)) {
             csvReader.iterator().forEachRemaining(order -> {
                 try {
                     if (returnIDs.contains(order[1])) {
-                        int rowID = Integer.parseInt(order[0]);
-                                                                //orderId, orderDate, shipDate, Ship Mode
-                        ShippingOrder shipOrder = new ShippingOrder(order[1],order[2],order[3], order[4]);
-                                                        //customerId, Name, Segment
-                        Customer customer = new Customer(order[5], order[6], order[7]);
-                                        //Country, City,     State,    Post Code,  Region
-                        Location location = new Location(order[8], order[9], order[10], order[11], order[12]);
-                                    //productId, category, sub-category, name
-                        Product product = new Product(order[13], order[14], order[15], order[16]);
-                        int sales = replaceCommaAndParse(order[17], rowID);
-                        int quantity = replaceCommaAndParse(order[18], rowID);
-                        int discount = replaceCommaAndParse(order[19], rowID);
-                        int profit = replaceCommaAndParse(order[20], rowID);
-                        Order fullOrder = new Order(rowID, shipOrder, customer, location,
-                        product, sales, quantity, discount, profit);
-                        allReturns.add(fullOrder);
+                        parseOrderObjects(allReturns, order);
                     }
-
                 } catch (NumberFormatException e) {
-                    System.err.println("Failed to parse data in Row ID: "
-                                        + order[0]);
+                    setErrorLabel(error, e.getMessage());
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    setErrorLabel(error, "Failed to parse data: index out of bounds.");
                 }
             });
         } catch (Exception e) {
-            System.err.println("Failed to read the Order CSV file.");
+            setErrorLabel(error, "Failed to read the Order CSV file.");
             e.printStackTrace();
         }
         return allReturns;
+    }
+
+    /**
+     * Creates a CSVReader instance for the specified file.
+     * 
+     * @param file   path to the CSV file to be read
+     * @param error  label to display the error message if an exception occurs
+     * @return       CSVReader instance
+     * @throws RuntimeException if exception occurs during creation of CSVReader
+     */
+    protected static CSVReader createCSVReader(String file, Label error) {
+        try {
+            return new CSVReaderBuilder(new FileReader(file))
+                    .withSkipLines(1)
+                    .withCSVParser(new CSVParserBuilder().withSeparator(';').build())
+                    .build();
+        } catch (FileNotFoundException e) {
+            setErrorLabel(error, "Failed to create CSV reader as\n\"" + file + "\"\n was not found.");
+            throw new RuntimeException( "Failed to create CSV reader for file: " + file, e);
+        }
+    }
+
+    /**
+     * Parses array of strings into an Order object and adds it to list.
+     * 
+     * @param list  the list to add the parsed Order object to
+     * @param order the array of strings to parse
+     */
+    protected static void parseOrderObjects(ArrayList<Order> list, String[] order) {
+        int rowID = Integer.parseInt(order[0]);
+        ShippingOrder shipOrder = //orderId, orderDate, shipDate, Ship Mode
+            new ShippingOrder(order[1],order[2],order[3], order[4]);
+        Customer customer = //customerId, Name, Segment
+            new Customer(order[5], order[6], order[7]);
+        Location location = //Country, City,  State,    Post Code,  Region
+            new Location(order[8], order[9], order[10], order[11], order[12]);
+        Product product = //productId, category, sub-category, name
+            new Product(order[13], order[14], order[15], order[16]);
+        int sales = replaceCommaAndParse(order[17], rowID);
+        int quantity = replaceCommaAndParse(order[18], rowID);
+        int discount = replaceCommaAndParse(order[19], rowID);
+        int profit = replaceCommaAndParse(order[20], rowID);
+        Order fullOrder = new Order(rowID, shipOrder, customer, location,
+        product, sales, quantity, discount, profit);
+        list.add(fullOrder);
+    }
+
+    /**
+     * Sets text of the Label to the provided message and makes Label visible.
+     * 
+     * @param error        the Label to display the error message
+     * @param errorMessage the error message to display
+     */
+    private static void setErrorLabel(Label error, String errorMessage) {
+        error.setText(errorMessage);
+        error.setVisible(true);
     }
 
     /**
@@ -129,9 +149,8 @@ public class CSVDataReader {
         try{
             return Integer.parseInt(orderItem.replace(",",""));
         }catch (NumberFormatException e){
-            System.err.println("Failed to parse string: " + orderItem
-                                + ", in row with id: " + rowID);
-            return -69;
+            throw new NumberFormatException("Failed to parse string: "
+                                    + orderItem + ", in row with id: " + rowID);
         }
     }
 }
